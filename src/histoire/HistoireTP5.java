33e68e29be93dbe24837598e351ca57711108818
NOTuglyduckling <osearle65@gmail.com>
2023-11-02 12:43:49 +0100

    hist TP5

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -3,14 +3,13 @@ package histoire;
 import personnages.principaux.*;
 
 public class HistoireTP5 {
-	
-
-	public HistoireTP5() {
-		// TODO Auto-generated constructor stub
-	}
 
 	public static void main(String[] args) {
-		
+		Commercant marco = new Commercant("Marco", 20);
+		Commercant chonin = new Commercant("Chonin", 40);
+		Commercant kumi = new Commercant("Kumi", 10);
+		Yakuza yaku = new Yakuza("Yaku Le Noir",30 ,"whisky", "Warsong");
+		Ronin roro = new Ronin("Roro",60 , "shochu");
 
 	}
 
